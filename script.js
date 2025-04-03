6d1202aa072494547607abb509c059ce14487785
Nikas <nsurgautas1@gmail.com>
2025-04-03 19:52:07 +0300

    caomon thsis have to be last one

--- script.js
+++ script.js
@@ -1,8 +1,3 @@
-// if ('serviceWorker' in navigator) {
-//   navigator.serviceWorker.register('/service-worker.js')
-//   .then(() => console.log("Service Worker registered!"))
-//   .catch(error => console.log("Service Worker registration failed:", error));
-// }
 
 
 
